b8e3284f29f807bef3d0296c610eb1f7f4b748f3
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-02 01:41:24 -0500

    initial commit

---
+++ src/test/java/org/cospina/test/springboot/app/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cospina.test.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
