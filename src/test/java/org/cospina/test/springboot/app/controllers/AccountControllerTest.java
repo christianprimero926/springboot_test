41cb87480bfc1430811704e2c0ccb236d1b936fb
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-09 00:40:36 -0500

    Update AccountControllerTest.java
    79. Escribiendo pruebas unitarias para el controlador con @WebMvcTest y MockMvc
    80. Escribiendo pruebas para el controlador parte 2

--- src/test/java/org/cospina/test/springboot/app/controllers/AccountControllerTest.java
+++ src/test/java/org/cospina/test/springboot/app/controllers/AccountControllerTest.java
@@ -1,12 +1,76 @@
 package org.cospina.test.springboot.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cospina.test.springboot.app.models.TransactionDTO;
+import org.cospina.test.springboot.app.services.AccountService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.cospina.test.springboot.app.Data.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(AccountController.class)
 class AccountControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private AccountService accountService;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
-    void detail() {
+    void testDetail() throws Exception {
+        // Given
+        when(accountService.findById(1L)).thenReturn(createAccount001().orElseThrow());
+
+        // When
+        mvc.perform(get("/api/accounts/1").contentType(MediaType.APPLICATION_JSON))
+        // Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.person").value("Andres"))
+                .andExpect(jsonPath("$.balance").value("1000"));
+
+        verify(accountService).findById(1L);
+    }
+
+    @Test
+    void testTransfer() throws Exception {
+        // Given
+        TransactionDTO dto = new TransactionDTO();
+        dto.setSourceAccountId(1L);
+        dto.setTargetAccountId(2l);
+        dto.setAmount(new BigDecimal("100"));
+        dto.setBankId(1L);
+
+        //When
+        mvc.perform(post("/api/accounts/transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+
+        // Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.message").value("Transferencia realizada con exito"))
+                .andExpect(jsonPath("$.transaction.sourceAccountId").value(1L));
+
     }
 }
