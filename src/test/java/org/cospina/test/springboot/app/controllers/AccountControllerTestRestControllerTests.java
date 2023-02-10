3829abdef5b580b4e434d134a9a26634e16a5d2d
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-10 17:39:52 -0500

    Update AccountControllerTestRestControllerTests.java
    96. Escribiendo primeras pruebas de integración parte 2

--- src/test/java/org/cospina/test/springboot/app/controllers/AccountControllerTestRestControllerTests.java
+++ src/test/java/org/cospina/test/springboot/app/controllers/AccountControllerTestRestControllerTests.java
@@ -1,5 +1,7 @@
 package org.cospina.test.springboot.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cospina.test.springboot.app.models.TransactionDTO;
 import org.junit.jupiter.api.*;
@@ -12,6 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,7 +36,7 @@ class AccountControllerTestRestControllerTests {
 
     @Test
     @Order(1)
-    void testTransfer() {
+    void testTransfer() throws JsonProcessingException {
         // Given
         TransactionDTO dto = new TransactionDTO();
         dto.setAmount(new BigDecimal("100"));
@@ -39,17 +44,35 @@ class AccountControllerTestRestControllerTests {
         dto.setTargetAccountId(2L);
         dto.setBankId(1L);
 
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "ok");
+        response.put("message", "Transferencia realizada con exito");
+        response.put("transaction", dto);
+
         // When
         ResponseEntity<String> responseEntity = client.
                 postForEntity(createUri("/api/accounts/transfer"), dto, String.class);
+
         System.out.println(port);
         String json = responseEntity.getBody();
-        assertNotNull(json);
         System.out.println(json);
+
         // Then
+        assertNotNull(json);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
         assertTrue(json.contains("Transferencia realizada con exito"));
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        assertEquals("Transferencia realizada con exito", jsonNode.path("message").asText());
+        assertEquals(LocalDate.now().toString(), jsonNode.path("date").asText());
+        assertEquals("100", jsonNode.path("transaction").path("amount").asText());
+        assertEquals(1L, jsonNode.path("transaction").path("sourceAccountId").asLong());
+
+        assertEquals(objectMapper.writeValueAsString(response), json);
+
+
     }
 
     private String createUri(String uri) {
